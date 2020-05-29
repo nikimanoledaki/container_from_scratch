eecc81356e8a997a4d2673a25670ba035d5af12c
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-29 13:49:31 +0100

    Creates child process with id 1

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("help")
 	}
@@ -20,13 +22,24 @@ func main() {
 func run() {
 	fmt.Printf("Running %v as pid %d\n", os.Args[2:], os.Getpid())
 
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v as pid %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	must(cmd.Run())
 }
